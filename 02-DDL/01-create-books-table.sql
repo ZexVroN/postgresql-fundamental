518e63874975bb4cf4ddd50c04b1699f13ca810e
Aether ZvN <zexvronofficial@gmail.com>
2026-08-02 22:18:21 +0700

    Document basic SQL CRUD examples

---
+++ 02-DDL/01-create-books-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+    id INTEGER,
+    title VARCHAR(255),
+    author VARCHAR(100),
+    publish_year INTEGER,
+    stock INTEGER
+);
---
+++ 03-DML/02-select-books.sql
@@ -0,0 +1,8 @@
+SELECT * FROM books;
+
+SELECT title
+FROM books;
+
+SELECT *
+FROM books
+WHERE stock = 8;
